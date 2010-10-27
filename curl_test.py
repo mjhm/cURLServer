ed5f14410ace03af1ff8ee69772379ac44cb1de1
John McLaughlin <johnmclaughlin@massanimation.com>
2010-10-27 13:05:54 -0700

    GAE Unit Tests.

--- curl_test.py
+++ curl_test.py
@@ -1,4 +1,3 @@
-
 #Copyright (c) 2010 John McLaughlin -- Mass Animation
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -17,7 +16,7 @@
 #AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
+#THE SOFTWARE. 
 
 from google.appengine.ext.webapp import util
 from google.appengine.ext import webapp
